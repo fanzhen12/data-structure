b843dc5885f003c11be2c9921dcc75e9c32440bc
fanzhen <fanzhen@300624.cn>
2025-10-29 16:28:32 +0800

    ADD: 开始std::vector的withoutstl版本的完整实现（非简化）

---
+++ container/std_vector_withoutstl_completeversion.cpp
@@ -0,0 +1,11 @@
+﻿// reference: https://www.doubao.com/chat/26654534756795138
+#include <stdexcept> // for std::out_of_range
+#include <utility> // for std::move, std::swap,std::forward
+#include <new> // for placement new
+#include <initializer_list> // for std::initializer_list(初始化列表)
+#include <iterator> // 迭代器相关类型
+
+template <typename T>
+class MyVector {
+
+};
